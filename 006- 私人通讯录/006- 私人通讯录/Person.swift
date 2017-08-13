89b1a7e3be26b62b86eb6484ef80c00b481b34c0
zhangyouwei <565393394@qq.com>
2017-08-13 15:31:36 +0800

    创建模型，异步加载数据，闭包回调

---
+++ 006- 私人通讯录/006- 私人通讯录/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  006- 私人通讯录
+//
+//  Created by 张小铀 on 2017/8/13.
+//  Copyright © 2017年 张小铀. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name:String?
+    var phone:String?
+    var title:String?
+    
+
+}
